c82bf88631489d7cc81f8587b7b7c46e1f242e48
Yavuz_Özbay <ozbayavz@gmail.com>
2021-11-20 23:06:39 +0100

    complex sort assigment created

--- week3/Complex Sort/myScript.js
+++ week3/Complex Sort/myScript.js
@@ -1,28 +1,33 @@
 // display the result
-console.log("The largest number is " + largest);
-let sayilar = [num1, num2, num3];
-sayilar.sort(function (a, b) {
-    return b - a
-});
-for (let sayi in sayilar) {
-    document.write(sayilar[sayi] + " ");
-}
+// 1)bir fonksiyon icinde girilen uc input alanindaki value degerlerine ulasan kod yazdik.
+//2) burada önemli olan value degerleri string oldugu icin integer'e cevrilmesi oldu. 
+//3)fonk. hemen altinda if else if Conditional Statements kodunu yazdim. 
+//4)elde ettigim sonuclari yazi ile ilgili dom kismina ulasip ekrana yazdirdim. 
+//5)console log ile console de  yazildi//
+
 
 function sayibulma() {
     let num1 = document.getElementById("sayi1").value;
+    let a = parseInt(num1);
     let num2 = document.getElementById("sayi2").value;
+    let b = parseInt(num2);
     let num3 = document.getElementById("sayi3").value;
+    let c = parseInt(num3);
     let largest = "unknown because you didn't put a valid.";
-
-    if (num1 >= num2 && num1 >= num3) {
-        largest = num1;
-        document.getElementById("result").innerHTML = largest
-    } else if (num2 >= num1 && num2 >= num3) {
-        largest = num2;
-        document.getElementById("result").innerHTML = largest
+    
+    if (a >= b && a >= c) {
+        largest = a;
+        document.getElementById("result").innerHTML = "large number is"+largest
+    } else if (b >= a && b >= c) {
+        largest = b;
+        document.getElementById("result").innerHTML = "large number is"+largest
     } else {
-        largest = num3;
-        document.getElementById("result").innerHTML = largest
+        largest = c;
+        document.getElementById("result").innerHTML = "large number is"+largest
     }
     console.log(result);
-}
+   // sayilari buyukten kucuge veya tam tersi siralamak yazilmis kod(reverse veya sort metodu)
+    let sayilar = [a, b, c];
+    sayilar.reverse(function(a, b){return a - b});
+    document.getElementById("siralama").innerHTML="buyukten kucuge"+sayilar
+    console.log("buyukten kucuge",sayilar)}
